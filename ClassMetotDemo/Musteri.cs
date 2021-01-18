1352bf39884799838f2cd5ada60af7ee1eb9004c
AbdullahCetin <AbdullahCetin@DESKTOP-5KHG1UU>
2021-01-18 17:24:36 +0300

    Class dosyarı oluşturma

--- ClassMetotDemo/Musteri.cs
+++ ClassMetotDemo/Musteri.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace ClassMetotDemo
 {
-    class Musteri
+    public class Musteri
     {
+
+
     }
 }
--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace ClassMetotDemo
 {
-    class MusteriManager
+    public class MusteriManager
     {
+
+
     }
 }
